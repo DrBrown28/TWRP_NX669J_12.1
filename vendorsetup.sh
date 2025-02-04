87601fecac14970a86cc5e922c516e6ccd460251
Shorif Uddin <gooogleabal@gmail.com>
2025-02-04 13:33:13 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,79 +1,62 @@
-#
-#	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2021-2022 The OrangeFox Recovery Project
-#
-#	OrangeFox is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	any later version.
-#
-#	OrangeFox is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#
-# 	This software is released under GPL version 3 or any later version.
-#	See <http://www.gnu.org/licenses/>.
-#
-# 	Please maintain this if you use this script or any part of it
-#
-
+# Allow missing dependencies
 export ALLOW_MISSING_DEPENDENCIES=true
 
+# Device codename
 FDEVICE="NX669J"
-#set -o xtrace
 
+# Function to get target device
 fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
-   if [ -n "$chkdev" ]; then 
-      FOX_BUILD_DEVICE="$FDEVICE"
-   else
-      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
-      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
-   fi
+    local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+    if [ -n "$chkdev" ]; then
+        FOX_BUILD_DEVICE="$FDEVICE"
+    else
+        chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+        [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+    fi
 }
 
+# If no device specified, try to determine the target
 if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
-   fox_get_target_device
+    fox_get_target_device
 fi
-    
+
+# Check if device matches
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
-export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+    # Use TWRP recovery image builder
+    export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+
+    # Locale settings
     export LC_ALL="C"
-  
-# Maintaining Info
-    export OF_MAINTAINER="Dr.Brown28"
+
+    # Build variables
+    export FOX_BUILD_TYPE="Unofficial"
     export FOX_VERSION=$(date +%y.%m.%d)
-    export FOX_BUILD_TYPE=Unofficial
-  	
-# Device Info
-    export FOX_ARCH=arm64
-    export FOX_VARIANT="12.1"
+    export FOX_ARCH="arm64"
     export TARGET_DEVICE_ALT="NX669J,NX69J-UN,NX669J-EEA,NX669S,NX669S-UN,NX669S-EEA"
-    
+
+    # A/B Partition support
     export FOX_AB_DEVICE=1
-    export FOX_REPLACE_BUSYBOX_PS=1
-    export FOX_REPLACE_TOOLBOX_GETPROP=1
-    export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
-    export FOX_DELETE_AROMAFM=1
+
+    # Status bar and screen settings
     export OF_SCREEN_H=2340
     export OF_STATUS_H=100
     export OF_STATUS_INDENT_LEFT=48
     export OF_STATUS_INDENT_RIGHT=48
+
+    # Disable navbar toggle
     export OF_ALLOW_DISABLE_NAVBAR=0
-    export OF_USE_LOCKSCREEN_BUTTON=1
-    export FOX_BUGGED_AOSP_ARB_WORKAROUND="1616300800"
+
+    # Additional flags
+    export FOX_DELETE_AROMAFM=1
     export FOX_USE_SPECIFIC_MAGISK_ZIP="~/magisk_canary_200122.zip"
-    export TW_DEVICE_VERSION="R11.1"
-    export FOX_VERSION=R11.1_0
     export OF_USE_TWRP_SAR_DETECT=1
 
-    # let's see what are our build VARs
+    # Debugging and log settings
     if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-       export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-       export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-       export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-       export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+        export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+        export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+        export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
     fi
+
 fi
